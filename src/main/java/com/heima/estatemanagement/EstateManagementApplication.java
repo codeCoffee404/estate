5147d8e764b2d304f68f90df66be4ad1eed4687a
CoffeeCode <977496475@qq.com>
2023-03-16 13:42:29 +0800

    项目准备，项目原型

---
+++ src/main/java/com/heima/estatemanagement/EstateManagementApplication.java
@@ -0,0 +1,22 @@
+package com.heima.estatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EstateManagementApplication.class, args);
+		System.out.println("  _                        \n" +
+				" | |                       \n" +
+				" | |   ___   __   __   ___ \n" +
+				" | |  / _ \\  \\ \\ / /  / _ \\\n" +
+				" | | | (_) |  \\ V /  |  __/\n" +
+				" |_|  \\___/    \\_/    \\___|\n" +
+				"                           \n" +
+				"                           ");
+	}
+
+}
